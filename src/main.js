a6c4a2f75225ba78cba03a1bfeec0b8aca59813d
Ktaewon <dydalehdxo@naver.com>
2021-12-29 09:50:40 +0900

    qrcode generator added

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const qrCode = require('qrcode')
+
+const PORT = 3000
+const app = express()
+app.use(express.static('./static'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/:qr', (req, res) => {
+  qrCode.toDataURL(req.params.qr, (err, url) => {
+    const data = url.replace(/.*,/, '')
+    const img = Buffer.from(data, 'base64')
+    res.writeHead(200, { 'Content-Type': 'image/png' })
+    res.end(img)
+  })
+})
+
+// @ts-ignore
+app.listen(PORT, (res, req) => {
+  console.log(`listening on Port ${PORT}`)
+})
